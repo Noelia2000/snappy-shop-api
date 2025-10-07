4f3ac621597daa6bb8274911d5cda1529156a245
Fernando Ricaldes <fernandorashed258@gmail.com>
2025-10-07 19:36:52 -0400

    Duplicado attributes

---
+++ shared-utils/attributesBase.js
@@ -0,0 +1,31 @@
+const baseAttributes = {
+  color: {
+    _id: "63f078f54b86ed26b05281b2",
+    type: "attribute",
+    extraType: "multiple",
+    status: "show",
+    title: { en: "Color", es: "Color" },
+    name: { en: "Color", es: "Color" },
+    option: "Dropdown",
+  },
+  giftWrap: {
+    _id: "63f34946d3639309840ca336",
+    type: "extra",
+    extraType: "multiple",
+    status: "show",
+    title: { en: "Gift Wrap", es: "Papel de Regalo" },
+    name: { en: "Gift Wrap", es: "Papel de Regalo" },
+    option: "Checkbox",
+  },
+  package: {
+    _id: "63f34983d3639309840ca64a",
+    type: "extra",
+    extraType: "multiple",
+    status: "show",
+    title: { en: "Package", es: "Paquete" },
+    name: { en: "Package", es: "Paquete" },
+    option: "Checkbox",
+  },
+};
+
+module.exports = baseAttributes;
